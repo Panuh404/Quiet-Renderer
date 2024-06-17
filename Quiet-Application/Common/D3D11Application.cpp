d1dd3ed4a119ad08c650c36403d88620cdc580a0
Panuh404 <40393106+Panuh404@users.noreply.github.com>
2024-06-16 23:18:11 -0300

    DX11 Empty Window

---
+++ Quiet-Application/Common/D3D11Application.cpp
@@ -0,0 +1,151 @@
+#include "D3D11Application.h"
+
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
+#include <DirectXMath.h>
+#include <d3dcompiler.h>
+
+#include <iostream>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "dxguid.lib")
+
+D3D11Application::D3D11Application(const std::string& title) : Application(title) {}
+
+D3D11Application::~D3D11Application()
+{
+}
+
+bool D3D11Application::Initialize()
+{
+	// Initializes GLFW and creates a Window
+	if (!Application::Initialize())
+	{
+		return false;
+	}
+	// Initializes DirectX's devices and SwapChain
+	if (FAILED(CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory))))
+	{
+		std::cerr << "DXGI: Failed to create factory\n";
+		return false;
+	}
+
+	constexpr D3D_FEATURE_LEVEL deviceFeatureLevel = D3D_FEATURE_LEVEL_11_0;
+	if (FAILED(D3D11CreateDevice(
+		nullptr,					// Display Adapter Interface
+		D3D_DRIVER_TYPE_HARDWARE,		// Driver Type
+		nullptr,					// Software
+		0,							// Runtime Flags
+		&deviceFeatureLevel,			// Out Feature level
+		1,					// Feature levels to attempt to create
+		D3D11_SDK_VERSION,				// SDK Version
+		&_device,						// Out Device created
+		nullptr,				// Out Feature level
+		&_deviceContext)))			// Out Device Context
+	{
+		std::cout << "D3D11: Failed to create device and device context\n";
+		return false;
+	}
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDescriptor = {};
+	swapChainDescriptor.Width = GetWindowWidth();
+	swapChainDescriptor.Height = GetWindowHeight();
+	swapChainDescriptor.Format = DXGI_FORMAT::DXGI_FORMAT_B8G8R8A8_UNORM;
+	swapChainDescriptor.SampleDesc.Count = 1;
+	swapChainDescriptor.SampleDesc.Quality = 0;
+	swapChainDescriptor.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDescriptor.BufferCount = 2;
+	swapChainDescriptor.SwapEffect = DXGI_SWAP_EFFECT::DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapChainDescriptor.Scaling = DXGI_SCALING::DXGI_SCALING_STRETCH;
+	swapChainDescriptor.Flags = {};
+
+	DXGI_SWAP_CHAIN_FULLSCREEN_DESC swapChainFullscreenDescriptor = {};
+	swapChainFullscreenDescriptor.Windowed = true;
+
+	if (FAILED(_dxgiFactory->CreateSwapChainForHwnd(
+		_device.Get(),
+		glfwGetWin32Window(GetWindow()),
+		&swapChainDescriptor,
+		&swapChainFullscreenDescriptor,
+		nullptr,
+		&_swapChain)))
+	{
+		std::cout << "DXGI: Failed to create SwapChain\n";
+		return false;
+	}
+
+	if (!CreateSwapchainResources())
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void D3D11Application::OnResize(int32_t width, int32_t height)
+{
+	Application::OnResize(width, height);
+	_deviceContext->Flush();
+
+	DestroySwapchainResources();
+	if (FAILED(_swapChain->ResizeBuffers(0, width, height, DXGI_FORMAT::DXGI_FORMAT_B8G8R8A8_UNORM, 0)))
+	{
+		std::cout << "D3D11: Failed to recreate SwapChain buffers\n";
+		return;
+	}
+	CreateSwapchainResources();
+}
+
+void D3D11Application::Render()
+{
+	D3D11_VIEWPORT viewport = {};
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = static_cast<float>(GetWindowWidth());
+	viewport.Height = static_cast<float>(GetWindowHeight());
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+
+	constexpr float clearColor[] = { 0.1f, 0.1f, 0.1f, 1.0f };
+
+	_deviceContext->ClearRenderTargetView(_renderTarget.Get(), clearColor);
+	_deviceContext->RSSetViewports(1, &viewport);
+	_deviceContext->OMSetRenderTargets(1, _renderTarget.GetAddressOf(), nullptr);
+	_swapChain->Present(1, 0);
+}
+
+void D3D11Application::Update()
+{
+}
+
+bool D3D11Application::Load()
+{
+	return true;
+
+}
+
+bool D3D11Application::CreateSwapchainResources()
+{
+	ComPtr<ID3D11Texture2D> backBuffer = nullptr;
+	if (FAILED(_swapChain->GetBuffer(0, IID_PPV_ARGS(&backBuffer))))
+	{
+		std::cout << "D3D11: Failed to get Back Buffer from the SwapChain\n";
+		return false;
+	}
+	if (FAILED(_device->CreateRenderTargetView(backBuffer.Get(), nullptr, &_renderTarget)))
+	{
+		std::cout << "D3D11: Failed to create RTV from Back Buffer\n";
+		return false;
+	}
+	return true;
+}
+
+void D3D11Application::DestroySwapchainResources()
+{
+	_renderTarget.Reset();
+}
---
+++ Quiet-Application/Common/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#include <iostream>
+
+#include "D3D11Application.h"
+
+//////////////////////////////////////////////////////////////////
+//	THIS IS TEST ONLY PROJECT									//
+//	THE FINAL IMPLEMENTATION OF THE RENDERER WILL BE IN QUIET	//
+//////////////////////////////////////////////////////////////////
+
+
+int main(int argc, char* argv[])
+{
+	D3D11Application app {"Quiet Renderer Test"};
+	app.Run();
+	return 0;
+}
+
